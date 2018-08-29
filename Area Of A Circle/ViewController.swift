060b2924d7f3db12a2fe32b2f9fef14f7b5093a7
Jehiel Pather <jspather@gmail.com>
2018-08-29 19:44:46 +1000

    Set up of UI

--- Area Of A Circle/ViewController.swift
+++ Area Of A Circle/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    var bannerView: GADBannerView!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textField.delegate = self
+        textField.keyboardType = UIKeyboardType.numbersAndPunctuation
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+  
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+       
+        return true
+       
+    }
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        let textFieldFloat = (textField.text as! NSString).floatValue
+        let answer: Float = textFieldFloat * textFieldFloat * Float.pi
+        answerLabel.text = String(answer)
+        print(answer)
+    }
+    
 
 }
 
